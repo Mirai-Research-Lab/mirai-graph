529f909ba1c21205480542d7730d4e8723d93b64
Debajyoti <debajyotidhar111@gmail.com>
2022-11-18 00:46:39 +0530

    modified subgraph.yaml

--- nft-marketplace/generated/IpfsNFT/IpfsNFT.ts
+++ nft-marketplace/generated/IpfsNFT/IpfsNFT.ts
@@ -158,9 +158,9 @@ export class Transfer__Params {
   }
 }
 
-export class Contract extends ethereum.SmartContract {
-  static bind(address: Address): Contract {
-    return new Contract("Contract", address);
+export class IpfsNFT extends ethereum.SmartContract {
+  static bind(address: Address): IpfsNFT {
+    return new IpfsNFT("IpfsNFT", address);
   }
 
   balanceOf(owner: Address): BigInt {
